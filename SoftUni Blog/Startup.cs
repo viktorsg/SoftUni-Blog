e152851c41e9af35eacb604285a169cfe15bcdd6
viktorsg <viktorsg@abv.bg>
2016-12-17 21:36:21 +0200

    Initial Commit

---
+++ SoftUni Blog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SoftUni_Blog.Startup))]
+namespace SoftUni_Blog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
